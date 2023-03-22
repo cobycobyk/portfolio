c7ed90b1a60bfd9a9f5aefa044935578abbf67c8
Coby Kabili <ckabili8@gmail.com>
2023-03-22 14:11:54 -0600

    header

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 type Props = {}
 
-function header({}: Props) {
+function Header({}: Props) {
   return (
-    <div>header</div>
+    <header className='sticky top-0 p-5 flex-items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+      <div className='flex flex-row items-center'>
+        <SocialIcon
+          url="https://www.youtube.com"
+          fgColor="gray"
+          bgColor='transparent'
+        />
+        <SocialIcon
+          url="https://www.youtube.com"
+          fgColor="gray"
+          bgColor='transparent'
+        />
+        <SocialIcon
+          url="https://www.youtube.com"
+          fgColor="gray"
+          bgColor='transparent'
+        />
+      </div>
+      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        <SocialIcon
+          className='cursor-pointer'
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+      </div>
+    </header>
   )
 }
 
-export default header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,7 @@ export default function Home() {
         <title>Cobys Portfolio</title>
       </Head>
       {/* header */}
+      <Header />
       {/* hero */}
       {/* about */}
       {/* experience */}
