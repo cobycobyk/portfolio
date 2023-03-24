09e7f756b00aa2903d2c0d57ac1970e24f17796c
Coby Kabili <ckabili8@gmail.com>
2023-03-24 12:33:16 -0600

    fetch utils

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,29 @@ import ContactMe from '@/components/ContactMe'
 import Link from 'next/link'
 import { ArrowUpIcon } from '@heroicons/react/24/solid'
 import Header from '@/components/Header'
+import { GetStaticProps } from 'next'
+import { Experience, PageInfo, Project, Skill, Social } from '@/typings'
+import { fetchPageInfo } from '@/utils/fetchPageInfo'
+import { fetchSocial } from '@/utils/fetchSocials'
+import { fetchExperiences } from '@/utils/fetchExperiences'
+import { fetchSkills } from '@/utils/fetchSkills'
+import { fetchProjects } from '@/utils/fetchProjects'
 
-const inter = Inter({ subsets: ['latin'] })
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience;
+  skills: Skill;
+  projects: Project[];
+  socials: Social[];
+}
 
-export default function Home() {
+export default function Home({pageInfo, experiences, skills, projects, socials}: Props) {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-y-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
         <title>Cobys Portfolio</title>
       </Head>
-      <Header />
+      <Header socials={socials} />
       <section id="hero" className='snap-start'>
         <Hero />
       </section>
@@ -48,4 +61,23 @@ export default function Home() {
       </Link>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocial();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 40,
+  }
 }
